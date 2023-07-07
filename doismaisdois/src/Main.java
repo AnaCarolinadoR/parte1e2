8427b93cea0684cdda7bc9470344fb82ed3de3df
AnaCarolinadoR <138242512+AnaCarolinadoR@users.noreply.github.com>
2023-07-06 22:57:40 -0300

    Add files via upload

---
+++ doismaisdois/src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in y
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2 + 2 = 4");
+
+        }
+    }
